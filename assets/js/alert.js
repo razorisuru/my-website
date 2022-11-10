6399c57d7b78401265a68a11fb1625dcea50fd29
Isuru Bandara <89684055+razorisuru@users.noreply.github.com>
2022-11-10 16:39:21 +0530

    Add files via upload

---
+++ assets/js/alert.js
@@ -0,0 +1,13 @@
+function showAlert() {
+    alert ("Under Development!");
+  }
+  
+  const element = document.getElementById("myBtn");
+  element.addEventListener("click", function() {
+    document.getElementById("alert1").innerHTML = "Hello World";
+  })
+  
+
+  
+  ;
+
